76bbe47622bb4114da9cd06ed778809b994d63de
piyushjaipuriar <142023718+piyushjaipuriar@users.noreply.github.com>
2023-12-04 14:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        let a = 0;
+        let b = 1;
+        for (let i = 2; i < num; i++) {
+            let temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
 }
-
-module.exports = fibonacci;
